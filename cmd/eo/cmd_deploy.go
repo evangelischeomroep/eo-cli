8f4cb98420a2c3e35ac3e5a6013783b75a44eab7
Edwin Boon <hello@edwinboon.dev>
2026-07-13 09:27:31 +0200

    add option to list all test webapps with nice huh select

--- cmd/eo/cmd_deploy.go
+++ cmd/eo/cmd_deploy.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/charmbracelet/huh"
 	"github.com/evangelischeomroep/eo-cli/internal/deploy"
 )
 
@@ -20,9 +21,53 @@ func cmdDeploy(args []string) error {
 		return err
 	}
 
-	for _, app := range apps {
-		fmt.Println(app.Name)
+	deployAll := false
+	for _, arg := range args {
+		if arg == "--all" || arg == "-a" {
+			deployAll = true
+		}
 	}
 
+	selected := apps
+	if !deployAll {
+		var err error
+		selected, err = pickDeploys(apps)
+		if err != nil {
+			return err
+		}
+	}
+
+	fmt.Println(selected)
 	return nil
+}
+
+func pickDeploys(apps []deploy.FunctionApp) ([]deploy.FunctionApp, error) {
+	names := make([]string, len(apps))
+
+	for i, app := range apps {
+		names[i] = app.Name
+	}
+
+	var selected []string
+	err := huh.NewForm(
+		huh.NewGroup(
+			huh.NewMultiSelect[string]().
+				Title("Select Function Apps to deploy").
+				Options(huh.NewOptions(names...)...).
+				Value(&selected),
+		),
+	).Run()
+	if err != nil {
+		return nil, err
+	}
+
+	var picked []deploy.FunctionApp
+	for _, app := range apps {
+		for _, s := range selected {
+			if app.Name == s {
+				picked = append(picked, app)
+			}
+		}
+	}
+	return picked, nil
 }
